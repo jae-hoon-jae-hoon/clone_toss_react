552fef40935a27329684a47b3eec6d1783e4e506
jaehoon <jeongjh159@naver.com>
2024-04-13 02:36:51 +0900

    메인페이지 설명 섹션 완료

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
                             </div>
                         </div>
                         <div className="intro__bottom">
-                            <button className="intro__bottom-btn">
+                            <button className="intro__bottom-btn" onClick={scroll_next_section}>
                                 <svg className="moving-svg"
                                     xmlns="http://www.w3.org/2000/svg"
                                     width="24"
@@ -67,8 +67,14 @@ function App() {
                 </section>
 
                 {/* Section - Description */}
-                <section className="main-sec" style={{ background: "pink" }}>
-                    asdasd
+                <section id="section_desc" className="main-sec desc">
+                    <div className="inner">
+                        <p>
+                            내 모든 금융 내역을 한눈에 조회하고 한 곳에서 관리하세요.<br />
+                            이제껏 경험 못 했던 쉽고 편리한 금융 서비스,<br />
+                            토스와 함께라면 당신의 일상이 새로워질 거예요.
+                        </p>
+                    </div>
                 </section>
 
                 {/* Section - 홈·소비 */}
@@ -85,6 +91,17 @@ function App() {
             {/* <footer>푸터메뉴 회사정보 정보처리방침 sns버튼</footer> */}
         </div>
     );
+}
+
+function scroll_next_section(){
+    let headerHeight = document.getElementById("header").offsetHeight
+    let sectionDesc = document.getElementById('section_desc').offsetTop;
+    let offsetTop = sectionDesc - headerHeight
+
+    window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth' // 부드러운 스크롤
+    });
 }
 
 export default App;
